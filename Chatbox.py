21db4a9f8f0b693e2863a0f112b5b26f836f8c1e
harryw2427 <67867112+harryw2427@users.noreply.github.com>
2020-07-05 10:49:54 -0400

    Add files via upload

---
+++ Chatbox.py
@@ -0,0 +1,82 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "What is your name?Harry\n",
+      "Nice to meet you, Harry!\n",
+      "What is your favorite food ?I don't have one\n",
+      "hmm ~~ I love I don't have one too ! That is yummy!!\n",
+      "What is your favorite sports? chess\n",
+      "Wow, that is impressive! I tried to learn chess too, but it is too difficult and I gave up..\n",
+      "How are you feeling today?bored\n",
+      "Why are you feeling bored now?\n",
+      "Please tell me..because i dont have anything to paly with\n",
+      "I understand ! Thanks for sharing\n",
+      "Goodbye Harry I liked chatting with you ! \n"
+     ]
+    }
+   ],
+   "source": [
+    "# coding: utf-8\n",
+    "\n",
+    "# In[1]:\n",
+    "\n",
+    "def introduce_myself():\n",
+    "    Name=input('What is your name?')\n",
+    "    print('Nice to meet you, '+Name+'!')\n",
+    "    Food=input('What is your favorite food ?')\n",
+    "    print('hmm ~~ I love '+Food +' too ! That is yummy!!')\n",
+    "    Sports=input('What is your favorite sports? ')\n",
+    "    print('Wow, that is impressive! I tried to learn '+Sports +' too, but it is too difficult and I gave up..')\n",
+    "    Feeling=input('How are you feeling today?')\n",
+    "    print('Why are you feeling '+Feeling,'now?')\n",
+    "    reason=input('Please tell me..')\n",
+    "    print('I understand ! Thanks for sharing')\n",
+    "    print('Goodbye',Name,'I liked chatting with you ! ')\n",
+    "\n",
+    "\n",
+    "# In[2]:\n",
+    "\n",
+    "introduce_myself()\n",
+    "\n",
+    "\n",
+    "# In[ ]:\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
